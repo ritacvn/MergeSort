a7725da247a64fbdded47cb106c673ccd2c5651b
ritacv <ritacv@motorola.com>
2020-06-19 13:06:40 -0400

    Initial Commit

---
+++ MergeSortExercicio/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MergeSortExercicio
+//
+//  Created by Rita de Cassia Vasconcelos do Nascimento on 19/06/20.
+//  Copyright © 2020 Rita de Cassia Vasconcelos do Nascimento. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
